b9b52baa56b5c860e77667c235879d54f2380f1c
Matheus Martins <matheus.mmn@hotmail.com>
2023-07-19 10:16:18 -0300

    ajuste de fonte

--- js/body/tags.js
+++ js/body/tags.js
@@ -7,13 +7,13 @@ $(window).on("load", function() {
         <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
             <div class="d-flex align-items-center bg-light mb-4" style="padding: 30px;">
                 <h1 class="fa fa-check cor_tag_meio m-0 mr-3"></h1>
-                <h5 class="font-weight-semi-bold tag_meio">Descrição</h5>
+                <h5 class="font-weight-semi-bold tag_meio tag_meio_texto">Descrição</h5>
             </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
             <div class="d-flex align-items-center bg-light mb-4" style="padding: 30px;">
                 <h1 class="fa fa-shipping-fast cor_tag_meio m-0 mr-2"></h1>
-                <h5 class="font-weight-semi-bold tag_meio">Entrega Rápida</h5>
+                <h5 class="font-weight-semi-bold tag_meio tag_meio_texto2">Entrega Rápida</h5>
             </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
